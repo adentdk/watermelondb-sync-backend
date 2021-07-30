828e462664ee6ac947638ddee866e3a3d36c9cd8
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2021-07-31 01:05:45 +0700

    adding getterMethods, beforeCreate Hooks

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -1,6 +1,9 @@
 'use strict'
 
+import { REGULAR_ROLE_ID } from '@src/constants'
+import bcrypt from '@src/utils/bcrypt'
 import { DataTypes, Model } from 'sequelize'
+import uuid from 'uuid-v4'
 import db from '../index'
 import Role from './Role'
 
@@ -27,16 +30,17 @@ User.init({
     allowNull: false,
     type: DataTypes.STRING(64)
   },
-  is_password_changed: {
-    allowNull: false,
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
   is_active: {
     allowNull: false,
     type: DataTypes.BOOLEAN,
     defaultValue: false,
   },
+  toResponse: {
+    type: DataTypes.VIRTUAL,
+    set: (value) => {
+      throw new Error('cannot set toResponse value')
+    }
+  }
 }, {
   sequelize: db.sequelize,
   name: 'User',
@@ -45,7 +49,40 @@ User.init({
   underscored: true,
   paranoid: true,
   timestamps: true,
-  deletedAt: 'deleted_at'
+  deletedAt: 'deleted_at',
+  getterMethods: {
+    async toResponse() {
+      const result = {
+        id: this.id,
+        name: this.name,
+        username: this.username,
+        email: this.email,
+        phone: this.phone,
+        is_password_changed: this.is_password_changed,
+        is_active: this.is_active,
+      }
+
+      const role = await this.getRole()
+
+      if (role) {
+        result.role = {
+          name: role.name
+        }
+      }
+
+      return result;
+    }
+  },
+  
+})
+
+User.beforeCreate(user => {
+  
+  user.id = uuid()
+  user.password = bcrypt.hash(user.password)
+
+  return user
+
 })
 
 User.Role = User.belongsTo(Role, {
