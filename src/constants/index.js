13681b33d71b29f9e46962445b9c31c332dbf5d9
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2021-07-31 01:04:26 +0700

    add REGULAR_ROLE_ID

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,7 +1,1 @@
-import config from "../config/config"
-
-export const SuperAdminRoleId = 1;
-export const CheckerRoleId = 3;
-export const ICRoleId = 5;
-export const DeveloperRoleId = 6;
-export const DefaultPassword = '123456'
+exports.REGULAR_ROLE_ID = 'eab196e7-9ba0-4776-98db-4423daf9c64c'
