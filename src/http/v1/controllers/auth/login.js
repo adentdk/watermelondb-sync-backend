0a62a88cf3edcc0cd78b7d07f91c20b4dd0ed31a
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2021-07-31 01:06:49 +0700

    auth login, register, init change password

---
+++ src/http/v1/controllers/auth/login.js
@@ -0,0 +1,78 @@
+import User from "@src/db/models/User";
+import bcrypt from "@src/utils/bcrypt";
+import jwt from "@src/utils/jwt";
+import { body } from "express-validator";
+import { StatusCodes } from "http-status-codes";
+
+const strings = {
+  success: 'Login Success',
+  error: 'Login Failed',
+  invalid: 'Username or Password is incorrect',
+  errorRequired: 'this field is required'
+}
+
+export const validationRules = [
+  body('username').exists().withMessage(strings.errorRequired),
+  body('password').exists().withMessage(strings.errorRequired)
+]
+
+export const execute = async (req, res) => {
+  const {
+    username,
+    password
+  } = req.body;
+
+
+  try {
+    const user = await User.findOne({
+      where: {
+        username,
+      },
+    })
+
+    if (user === null) {
+      throw new Error(strings.invalid)
+    }
+
+    const isPasswordMatch = bcrypt.compare(password, user.password);
+    if (isPasswordMatch) {
+      const role = await user.getRole();
+
+      const jwtPayload = {
+        uid: user.id,
+        role_id: role.id
+      }
+
+      const accessToken = jwt.signToken(jwtPayload);
+
+      user.is_active = true;
+
+      await user.save();
+
+      const userResponse = await user.toResponse
+
+      return res.status(StatusCodes.OK).json({
+        message: strings.success,
+        accessToken,
+        user: userResponse
+      }).end()
+    } else {
+      throw new Error(strings.invalid)
+    }
+
+  } catch (error) {
+
+    if (error.message === strings.invalid) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: strings.error,
+        error: [{
+          param: '_global',
+          message: error.message
+        }]
+      }).end()
+    } else {
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+
+  }
+}
---
+++ src/http/v1/controllers/auth/register.js
@@ -0,0 +1,80 @@
+import { REGULAR_ROLE_ID } from '@src/constants'
+import User from '@src/db/models/User'
+import jwt from '@src/utils/jwt'
+import { logger } from '@src/utils/logger'
+import { body } from 'express-validator'
+import { StatusCodes } from 'http-status-codes'
+
+const strings = {
+  success: 'Register success',
+  error: 'Register failed',
+  invalid: 'Username is already taken',
+  errorRequired: 'this field is required'
+}
+
+export const validationRules = [
+  body('name').exists().withMessage(strings.errorRequired),
+  body('username')
+    .exists()
+    .withMessage(strings.errorRequired)
+    .custom(async (value) => {
+      try {
+        if (value) {
+          const user = await User.findOne({ where: { username: value } })
+          if (user === null) {
+            return Promise.resolve();
+          } else {
+            throw new Error()
+          }
+        } else {
+          throw new Error()
+        }
+      } catch (error) {
+        return Promise.reject()
+      }
+
+    })
+    .withMessage(strings.invalid),
+  body('password').exists().withMessage(strings.errorRequired),
+]
+
+export const execute = async (req, res) => {
+  const {
+    username,
+    password,
+    name
+  } = req.body;
+
+  try {
+    const user = await User.create({
+      username,
+      password,
+      name,
+    })
+
+    await user.setRole(REGULAR_ROLE_ID);
+
+    const jwtPayload = {
+      uid: user.id,
+      role_id: REGULAR_ROLE_ID
+    }
+
+    const accessToken = jwt.signToken(jwtPayload);
+
+    user.is_active = true;
+
+    await user.save();
+
+    const userResponse = await user.toResponse
+
+    return res.status(StatusCodes.OK).json({
+      message: strings.success,
+      accessToken,
+      user: userResponse
+    }).end()
+
+  } catch (error) {
+    logger.error(error.message)
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+  }
+}
